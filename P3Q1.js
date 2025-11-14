c031fb9f753822ade88bcc7fc334e08de78c232b
Jeff Teoh <jeff.teoh.01@gmail.com>
2025-11-14 16:43:09 +1100

    P3Q1 - Define Strings

---
+++ P3Q1.js
@@ -0,0 +1,9 @@
+let myString = "this is a string";
+let anotherString = "   Another string";
+let hello = "Hello there!";
+let myName = "Jeff";
+
+console.log(myString);
+console.log(anotherString);
+console.log(hello);
+console.log(myName);
