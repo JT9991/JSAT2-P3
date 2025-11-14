d7575d3fbf8a23966cbe7e6767b4a4dbd348fd80
Jeff Teoh <jeff.teoh.01@gmail.com>
2025-11-14 16:58:49 +1100

    P3Q2 - Util Functions

--- P3Q1.js
+++ P3Q1.js
@@ -7,3 +7,18 @@ console.log(myString);
 console.log(anotherString);
 console.log(hello);
 console.log(myName);
+
+
+// length of myString
+let lengthOfMyString = myString.length;
+
+// First character of myString
+let firstCharacter = myString.charAt(0);
+
+// 11th Character of myString
+let eleventhCharacter = myString.charAt(10);
+
+
+console.log(lengthOfMyString);
+console.log(firstCharacter);
+console.log(eleventhCharacter);
